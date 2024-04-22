092a547a785df740c39c6e9008b222fc4efbad1c
PrafulGupta85 <praful.gupta@grazitti.com>
2024-04-22 17:02:17 +0530

    dds

---
+++ JS/first/05String.js
@@ -0,0 +1,38 @@
+// const name = "Hitesh Rajan";
+// const anotherName = new String("Modi Singh");
+
+// console.log(name);
+// console.log(anotherName);
+
+// const string = '"Geeks" For Geeks';
+// console.log(string);
+
+// let str = "Mind power soul and mind in power and soul";
+// let part = str.replaceAll("power", "mice");
+
+// console.log(str);
+// str.toUpperCase();
+// console.log(str);
+
+// str.toLowerCase();
+// console.log(str);
+
+// let str = "      Soutl      ";
+
+// str.trimStart();
+// console.log(str);
+// str = str.trim();
+// console.log(str);
+
+// let str = "Hello-how are-you are you -doing- good";
+// console.log(str.split("-"));
+
+// let str = "hey how are you";
+
+// console.log(str.search("s"));
+
+let str = "Hello how are you , do you know hello and how ";
+let newstr;
+newstr = str.replaceAll("how", "efhadfadfadfad");
+console.log(str);
+console.log(newstr);
