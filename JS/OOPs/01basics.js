e7ac89808b9e53aff819ae700e62531ebf0670ba
PrafulGupta85 <praful.gupta@grazitti.com>
2024-04-30 10:39:38 +0530

    New commit

--- JS/OOPs/01basics.js
+++ JS/OOPs/01basics.js
@@ -1,11 +1,11 @@
 // This object is created using Object Literal
-let person = {
+let demo = {
   fName: "Praful",
   lName: "Gupta",
 
   //method
   getFunction: function () {
-    return `This is Object person and First Name ${this.fName} and Last Name ${this.lName}`;
+    return `This is Object demo and First Name ${this.fName} and Last Name ${this.lName}`;
   },
 
   // object in object
@@ -15,9 +15,8 @@ let person = {
     area: "UP",
   },
 };
-
-// console.log(person.getFunction());
-// console.log(person.phoneNo.area, person.phoneNo.number);
+// console.log(demo.getFunction());
+// console.log(demo.phoneNo.area, demo.phoneNo.number);
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -34,5 +33,80 @@ class Vehicle {
 let audiQ8 = new Vehicle("Q8", "Audi", 3000);
 let bmwx1 = new Vehicle("x1", "Bmw", 2800);
 
-console.log(bmwx1.getDetails());
-console.log(audiQ8.getDetails());
+// console.log(bmwx1.getDetails());
+// console.log(audiQ8.getDetails());
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// This is similar to above example but here we are using OOPS properties
+// Here we use encapsulation to wrap properties and fun.
+class PersonOne {
+  constructor(name, id, mail) {
+    (this.name = name), (this.id = id), (this.mail = mail);
+  }
+
+  setAddress(add) {
+    this.add = add;
+  }
+
+  getOutput() {
+    return `Person Name: ${this.name}, id: ${this.id}, mail: ${this.mail} and address: ${this.add}`;
+  }
+}
+
+let Ajay = new PersonOne("Ajay", 1, "ajay@su.com");
+Ajay.setAddress("Palampur, India");
+// console.log(Ajay.getOutput());
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Abstraction example
+// By changing the way we define a function we can restrict its scope.
+
+function person(fname, lname) {
+  let firstname = fname;
+  let lastname = lname;
+
+  let getDetails_noaccess = function () {
+    return `First name is: ${firstname} Last 
+			name is: ${lastname}`;
+  };
+
+  this.getDetails_access = function () {
+    return `First name is: ${firstname}, Last name is: ${lastname}`;
+  };
+}
+let person1 = new person("Mukul", "Latiyan");
+// console.log(person1.firstname); // undefined
+// console.log(person1.getDetails_noaccess); // undefined
+// console.log(person1.getDetails_access()); // shows output
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Here we are using object literals to create the object
+let car = {
+  name: "GT",
+  maker: "BMW",
+  enginer: "1998cc",
+  start: function () {
+    console.log("Starting the engine....");
+    console.log(this);
+  },
+};
+car.brakesType = "All Disc"; // add property to class
+car.start();
+
+car.stop = function () {
+  console.log("Turning the Engine off...");
+  console.log(this);
+};
+car.stop();
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// JSON Objects
+
+// const person = {
+//     "name" : "Jhon",
+//     "age" : 35,
+//     "profession" : "Wrestler"
+// }
+// JSON.parse(person);  // This converts JSON object to Javascript object
---
+++ JS/OOPs/02basics.js
@@ -0,0 +1,35 @@
+// Constructor Function
+
+function User(username, email, isLoggedIn) {
+  (this.username = username),
+    (this.email = email),
+    (this.isLoggedIn = isLoggedIn);
+
+  this.greet = function () {
+    console.log(`Hello ${this.username}, email: ${this.email}`);
+    console.log(this);
+  };
+}
+
+const userOne = new User("Yash", "yash@gz.com", true);
+userOne.greet();
+
+function Car(carName, carBrand, carPrice) {
+  (this.carName = carName),
+    (this.carBrand = carBrand),
+    (this.carPrice = carPrice);
+
+  this.getDetails = function () {
+    return `The ${carName} is of ${carBrand} company and have price of ${carPrice} `;
+  };
+  this.setCarBrand = function (carBrand) {
+    this.carBrand = carBrand;
+  };
+}
+
+const bmwCar = new Car("730d", "BMW", 70);
+
+bmwCar.setBrand("Volvo");
+bmwCar.getDetails();
+
+const audiCar = new Car("q3", "Audi", 90);
