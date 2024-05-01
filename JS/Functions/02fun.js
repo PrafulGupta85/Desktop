c2f3547e01e597c550cf6ffdeca6ed4a32e64e30
PrafulGupta85 <praful.gupta@grazitti.com>
2024-05-01 12:41:46 +0530

    new -c

---
+++ JS/Functions/02fun.js
@@ -0,0 +1,59 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// const tF = (noOfChildren, partnerName, geoLocn, jobTitle)=>{
+//     return `You will be a ${jobTitle} in ${geoLocn}, and married to ${partnerName} with ${noOfChildren} kids.`
+// }
+
+// let n = prompt("Enter the number of argumnets : ");
+
+// while(n--){
+//     console.log(`-------------------------------------`);
+//     let children = prompt("Enter number of Children: ");
+//     let partnerName = prompt("Enter number of Partner Name: ");
+//     let geoLocn = prompt("Enter Geographical Location: ");
+//     let jobTitle = prompt("Enter your Job Title: ");
+
+//     console.log(tF(children, partnerName, geoLocn, jobTitle));
+// }
+
+//---------------------------------------------------------//
+
+// const calculateDogAge = (age)=>{
+//     let dogAge = age*7
+//     return `Your doggie is ${dogAge} years old in dog years!`
+// }
+
+// let age = prompt("Enter the dog age: ");
+// console.log(calculateDogAge(age));
+
+//----------------------------------------------------------//
+
+// const calcGeometry = (radius)=>{
+//   let circumference = (Math.PI * 2*radius).toFixed(2);
+//   console.log(`The circumference is ${circumference}`);
+//   let area = (Math.PI * radius*radius).toFixed(2);
+//   console.log(`The area is ${area}`);
+// }
+
+// let radius = prompt("Enter radius: ");
+// calcGeometry(radius);
+
+//----------------------------------------------------------//
+//------------- Prompt run in online compiler or attach js file with html ---------------//
+
+// const celsiusToFahrenheit = (celsius) => {
+//   const celsiusInF = (celsius * 9) / 5 + 32;
+//   console.log(`${celcius}°C is ${celsiusInF}°F`);
+// };
+
+// const fahrenheitToCelsius = (fahrenheit) => {
+//   const fahrenheitInC = ((fahrenheit - 32) * 5) / 9;
+//   console.log(`${fahrenheit}°F is ${fahrenheitInC}°C`);
+// };
+
+// const celcius = prompt("Enter temp in celcius: ");
+// celsiusToFahrenheit(celcius);
+
+// const fahren = prompt("Enter temp in faren: ");
+// fahrenheitToCelsius(fahren);
